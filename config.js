53383c96451130feb397134ec02220d895c540b7
DataLioness <d.opdkamp@gmail.com>
2025-07-23 21:12:30 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,6 @@
 var title = "Server name";
 var link = "Zua is gay";
 //---------------End Config---------------
-
-
-document.getElementById('title').innerHTML = title;
-document.getElementById('link').innerHTML = link;
  
 
 console.log('Connected.')
