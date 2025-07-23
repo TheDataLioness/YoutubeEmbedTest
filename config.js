87a6b25a49b8b1d6156407d2d727672f97fb99ba
DataLioness <d.opdkamp@gmail.com>
2025-07-23 22:00:41 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,11 @@
+function querystring(key) {
+   var re=new RegExp('(?:\\?|&)'+key+'=(.*?)(?=&|$)','gi');
+   var r=[], m;
+   while ((m=re.exec(document.location.search)) != null) r[r.length]=m[1];
+   return r;
+}
+
+var urlvar = querystring("url")[0];
+
+document.getElementById("iframe_embed").url = urlvar;
 
